a18ab280a7733ee0b8c27b20d27b86b521a1271e
ArinaGorbacheva <43599396+ArinaGorbacheva@users.noreply.github.com>
2020-04-23 12:50:35 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ import io
 
 from setuptools import find_packages, setup
 
-#with io.open('README.rst', 'rt', encoding='utf8') as f:
-#    readme = f.read()
-
 setup(
     name='js_example',
     version='1.0.0',
@@ -13,7 +10,6 @@ setup(
     maintainer='Pallets team',
     maintainer_email='contact@palletsprojects.com',
     description='Demonstrates making Ajax requests to Flask.',
-#    long_description=readme,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
